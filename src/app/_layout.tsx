41fa648e5d9477b2e310772903c94a820f962f65
Leticia Nobre <leticiavidal2910@gmail.com>
2024-12-09 21:54:25 -0300

    refat: config fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,25 @@
 import { Stack } from "expo-router"
 import { colors } from "@/styles/colors"
 
+import {useFonts,
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold
+} from "@expo-google-fonts/rubik"
+
 export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Rubik_600SemiBold,
+        Rubik_400Regular,
+        Rubik_500Medium,
+        Rubik_700Bold
+    })
+
+    if(!fontsLoaded) {
+        return
+    }
+    
     return <Stack screenOptions={{
         headerShown: false,
         contentStyle: { backgroundColor: colors.gray[100] }
