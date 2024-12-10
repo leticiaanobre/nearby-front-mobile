480008908fe8d802478ae57ba35e6ed443f69b5d
Leticia Nobre <leticiavidal2910@gmail.com>
2024-12-09 21:30:24 -0300

    style: added colors and style files

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,10 @@ import {View, Text} from "react-native"
 
 export default function Index(){
     return(
-        <View>
+        <View style={{
+            flex: 1, 
+            justifyContent: "center", 
+            alignItems: "center"}}>
             <Text>Hello React Native</Text>
         </View>
     )
