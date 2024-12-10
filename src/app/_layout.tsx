c6571f37fa5408856fccc98011edf18cfda8b253
Leticia Nobre <leticiavidal2910@gmail.com>
2024-12-09 21:45:26 -0300

    config: config files adjustment

---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router"
+import { colors } from "@/styles/colors"
+
+export default function Layout() {
+    return <Stack screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: colors.gray[100] }
+    }} />
+}
