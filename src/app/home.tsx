45940c3f818d6c88db621feba2eb5ec88ba5f23f
Leticia Nobre <leticiavidal2910@gmail.com>
2024-12-11 07:44:11 -0300

    feat: added dependencies and call api

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,6 +1,20 @@
-import { View, Text } from "react-native";
+import { api } from "@/services/api";
+import { View, Text, Alert } from "react-native";
+import { useEffect } from "react";
 
 export default function Home() {
+    async function fetchCategories() {
+        try {
+            const {data} = await api.get("/categories")
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Categorias", "Não foi possível carregar as categorias")
+        }
+    }
+    useEffect(() => {
+        fetchCategories()
+    }, [])
     return (
         <View style={{flex: 1}}>
             <Text>Home</Text>
