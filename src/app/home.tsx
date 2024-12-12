46ee049c77eba0ec747d2a2b90730463e69d16b3
Leticia Nobre <leticiavidal2910@gmail.com>
2024-12-12 18:18:50 -0300

    feat: added fetch markets

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,10 +2,15 @@ import { api } from "@/services/api";
 import { View, Text, Alert } from "react-native";
 import { useEffect, useState } from "react";
 import { Categories, CategoriesProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
+
+type MarketsProps = PlaceProps 
 
 export default function Home() {
     const [categories, setCategories] = useState<CategoriesProps>([])
     const [category, setCategory] = useState("")
+    const [markets, setMarkets] = useState<MarketsProps[]>([])
+
     async function fetchCategories() {
         try {
             const {data} = await api.get("/categories")
@@ -16,9 +21,31 @@ export default function Home() {
             Alert.alert("Categorias", "Não foi possível carregar as categorias")
         }
     }
+
+    async function fetchMarkets() {
+        try {
+            if(!category) {
+                return
+            }
+
+            const {data} = await api.get("/markets/category/" + category)
+            setMarkets(data)
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Locais", "Não foi possível carregar os locais.")
+            
+        }
+    }
+
     useEffect(() => {
         fetchCategories()
     }, [])
+
+    useEffect(() => {
+        fetchMarkets()
+    }, [])
+
     return (
         <View style={{flex: 1}}>
             <Categories data={categories}  onSelect={setCategory} selected={category}/>
