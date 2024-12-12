f986e386162f9b8a9b017ee3083a9e71b125bd1e
Leticia Nobre <leticiavidal2910@gmail.com>
2024-12-12 18:41:14 -0300

    refat: adjusted places component

---
+++ src/components/places/index.tsx
@@ -0,0 +1,21 @@
+import { Text, useWindowDimensions } from "react-native";
+import { Place, PlaceProps} from "../place";
+import BottomSheet, {BottomSheetFlatList} from "@gorhom/bottom-sheet"
+import { useRef } from "react";
+
+type Props = {
+    data: PlaceProps[]
+}
+
+export function Places({ data}: Props) {
+    const dimensions = useWindowDimensions()
+    const bottomSheetRef = useRef<BottomSheet>(null)
+    return(
+        <BottomSheet>
+            <BottomSheetFlatList 
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) => <Place data={item} />}/>
+        </BottomSheet>
+    )
+}
