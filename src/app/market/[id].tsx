c7ab5ed8113df29c5e7b5e6c7be595ca97f7c5b0
Leticia Nobre <leticiavidal2910@gmail.com>
2024-12-15 20:51:54 -0300

    feat: created cupon and added style

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -6,6 +6,7 @@ import { api } from "@/services/api";
 import { Loading } from "@/components/loading";
 import { Cover } from "@/components/market/cover";
 import { Details, PropsDetails } from "@/components/market/details";
+import { Cupon } from "@/components/market/cupon";
 
 type DataProps = PropsDetails & {
     cover: string
@@ -49,6 +50,7 @@ export default function Market() {
         <View style={{flex: 1}}>
             <Cover uri={data.cover}/>
             <Details data={data}/>
+            <Cupon code="FM4345T6" />
         </View>
     )
 }
